9d8756ce0834796826c092f85590694cc0dcda27
aferraz <at_aferraz@ilstu.edu>
2020-01-27 12:42:12 -0600

    taskbox UI

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
-</script>
-
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -25,4 +7,25 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+<template>
+  <div id="app">
+    <inbox-screen />
+  </div>
+</template>
+
+<script>
+  import store from './store';
+  import InboxScreen from './components/InboxScreen.vue';
+  export default {
+    name: 'app',
+    store,
+    components: {
+      InboxScreen,
+    },
+  };
+</script>
+<style>
+  @import './index.css';
 </style>
